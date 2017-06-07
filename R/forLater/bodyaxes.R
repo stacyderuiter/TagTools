157f16e799433211c6003147fdcf2aec149a07e0
dasweeney4423 <dasweeney4423@gmail.com>
2017-06-07 15:57:18 -0400

    UNFINISHED bodyaxes.m translated into R code

---
+++ R/forLater/bodyaxes.R
@@ -0,0 +1,93 @@
+# The following is an attempt to translate Mark Johnson's bodyaxes.m matlab script, from the dtag tool box, to an R script. Note that we made no
+# effort to vectorize or use apply...just changed matlab to R code keeping the same general structure. DAS and YJO, June 2017
+
+# function  W = bodyaxes(A, M, fc)
+
+#     W = bodyaxes(A,M,fc)
+
+#     Generate the cardinal axes of an animal (i.e., the longitudinal, transverse,
+#     and ventro-dorsal) from accelerometer and magnetic field measurements. This
+#		  functions generates an approximate orthonormal basis from each measurement of
+#		  A and M by:
+#      (i)   normalizing A and M to unit length
+#      (ii)  rotating the magnetometer measurement to the horizontal plane (Mh), 
+#      (iii) computing the cross-product, N, of A and Mh to generate the third axis, 
+#      (iv)  transposing [Mh,N,A] to form the body axis basis.
+#
+#     Inputs:
+#      A is the acceleration matrix with columns [ax ay az]. Acceleration can 
+#		    be in any consistent unit, e.g., g or m/s^2. 
+#      M is the magnetometer signal matrix, M=[mx,my,mz] in any consistent unit
+#		   (e.g., in uT or Gauss).
+#	     fc (optional) specifies the cut-off frequency of a low-pass filter to
+#		    apply to A and M before computing the axes. The filter cut-off
+#		    frequency is with respect to 1=Nyquist frequency. The filter length is
+#		    8/fc. Filtering adds no group delay. If fc is not specified, no filtering 
+#		    is performed.
+#
+#     Returns:
+#    W is the 3x3xn matrix of body axes where n is the number of rows in M and A.
+#		 W(:,1,:) are the X or longitudinal (caudo-rostral) axes. 
+#		 W(:,2,:) are the Y or transverse (left-right) axes.
+#		 W(:,3,:) are the Z or ventro-dorsal axes.
+#
+#   Output sampling rate is the same as the input sampling rate.
+#	  	Frame: This function assumes a [north,east,up] navigation frame and a
+#		  [forward,right,up] local frame. This function will only return the
+#		  animal's cardinal axes if the tag was attached so that the
+#		  sensor axes aligned with the animal's axes OR if the tag A and M measurements
+#		  are rotated to account for the orientation of the tag on the animal (see
+#  	  tagorientation() and tag2animal() to do this). Otherwise, the axes returned
+#		  by this function will be the cardinal axes of the tag, not the animal.
+#
+#		Example:
+# 		 W = bodyaxes(matrix(c(-0.3, 0.52, 0.8, 22, -22, 14), byrow = TRUE, nrow = 2, ncol = 3)
+#  	     returns: W=[0.59682  -0.55182   0.58249
+#                    0.74420   0.65208  -0.14477
+#                   -0.29994   0.51990   0.79984]
+# 
+#  Valid: Matlab, Octave
+#  markjohnson@st-andrews.ac.uk
+#  Last modified: 10 May 2017
+
+bodyaxes <- function(A, M, fc) {
+  if (nargs() <2) {
+    help(bodyaxes)
+  }
+  require(matlab) #for size(), repmat(), and zeros() functions
+  if (size(M, 1) * size(M, 2) == 3) {
+    M <- t(M)
+  }
+  if (size(A, 1) * size(A,2) == 3) {
+    A <- t(A)
+  }
+  if (size(A, 1) != size(M, 1)) {
+    print(sprintf('bodyaxes: A and M must have same number of rows \ n'))
+  }
+  if is.null(fc) {
+    fc <- .2
+  }
+  if (nargs() == 3 & size(A, 1) > (8 / fc)) {
+    M <- fir_nodelay(M, round(8 / fc), fc)
+    A <- fir_nodelay(A, round(8 / fc), fc)
+  }
+  b <- sqrt(rowSums(M^2))
+  g <- sqrt(rowSums(A^2))
+  #normalize M to unit magnitude
+  M <- M * repmat(b^(-1), 1, 3)
+  #normalize A to unit magnitude
+  A <- A * repmat(g^(-1), 1, 3)
+  #estimate inclination angle from the data
+  I <- acos(rowSums(A * M)) - (pi / 2)
+  Mh <- (M + repmat(sin(I), 1, 3) * A) * repmat(cos(I)^(-1), 1, 3)
+  v <- sqrt(rowSums(Mh^2))
+  #normalize Mh
+  Mh <- Mh * repmat(v^(-1), 1, 3) 
+  require(RSEIS)  #forxprod() function
+  #for FRU axes
+  N <- (-xprod(Mh, A, 2))
+  W <- zeros(3, 3, size(A, 1))
+  W[1,,] <- t(M)
+  W[2,,] <- t(N)
+  W[3,,] <- t(A)
+}
