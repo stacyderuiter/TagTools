79701c557b70c0bd4cb6c9371c2c28e5f19045fe
unknown <yo24@PC51195.ad.calvin.edu>
2017-06-09 14:35:52 -0400

    This is the R implementation of a method used in speclev, called buffer. Works like buffer with opt = 'nodelay'

---
+++ R/forLater/buffer_nodelay.R
@@ -0,0 +1,26 @@
+buffer_nodelay <- function(vec,n,p){
+  m <- floor((length(vec) - n)/(n - p)) + 1
+  buffermatrix <-function(vec, m, n, p){
+  retmat <- matrix(0L, nrow = m, ncol = n)
+  vecindex <- 1
+  for(i in 1:m){
+    if(i == 1){
+    for(f in 1:n){
+         retmat[i,f] <- vec[vecindex]
+        vecindex <- vecindex + 1
+      }
+    }
+    else{
+      vecindex <- vecindex - p
+      for(c in 1:n){
+        retmat[i,c] <- vec[vecindex]
+        vecindex <- vecindex + 1
+      }
+      }
+  }
+  return(retmat)
+  }
+  realretmat <- matrix(0L, nrow = n, ncol = m)
+  realretmat <- t(buffermatrix(vec, m,n,p))
+  return(realretmat)
+}
