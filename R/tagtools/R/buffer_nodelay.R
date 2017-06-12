068c0a55d8c997efd2ad5d94fdadc17bbc179d26
dasweeney4423 <dasweeney4423@gmail.com>
2017-06-12 15:40:27 -0400

    added to NAMESPACE

--- R/tagtools/R/buffer_nodelay.R
+++ R/tagtools/R/buffer_nodelay.R
@@ -4,6 +4,7 @@
 #' @param n The desired length of data segments (rows).
 #' @param p The desired amount of overlap between consecutive frames (columns) in the output matrix
 #' @return A matrix of the buffered signal vector "vec" with "n" data segments and an overlap between consecutive frames specified by "p".
+#' @export
 #' @example vec <- c(1:20)
 #'          n <- 5
 #'          p <- 2
