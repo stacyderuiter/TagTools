ec3df552f75eb9bd5d5a1bbeecf8142dc021134b
dasweeney4423 <dasweeney4423@gmail.com>
2017-06-13 17:17:14 -0400

    answer does not match matlab

---
+++ R/forLater/rms_in_blocks.R
@@ -0,0 +1,31 @@
+#' Compute the RMS (root-mean-square) of successive blocks of samples.
+#' 
+#' @param X A vector or a matrix containing samples of a signal in each column.
+#' @param n The number of samples from X to use in each analysis block.
+#' @param nov The number of samples that the next block overlaps the previous block.
+#' @return Y A vector or matrix containing the RMS value of each block. If X is a mxn matrix, Y is pxn where p is the number of complete n-length blocks with nov that can be made out of m samples, i.e., n+(p-1)*(n-nov) < m
+#' @note Output sampling rate is the same as the input sampling rate so s and v have the same size as p.
+#' @note Frame: This function assumes a [north,east,up] navigation frame and a [forward,right,up] local frame. In these frames, a positive pitch angle is an anti-clockwise rotation around the y-axis. A descending animal will have a negative pitch angle.
+#' @export
+
+rms_in_blocks <- function(X, n, nov = NULL) {
+  if (is.null(nov) == TRUE) {
+    nov <- 0
+  }
+  #catch the case of a row vector input
+  if (nrow(X) == 1) {
+    X <- t(X)
+  }
+  nov <- pmin(n, nov)
+  S <- abs(X^2)
+  ss <- buffer_nodelay(S[,1], n, nov)
+  Y <- matrix(0, nrow = ncol(ss), ncol = ncol(X))
+  Y[, 1] <- t(sum(ss))
+  for (k in 2:ncol(X)) {
+    ss <- buffer_nodelay(S[,k], n, nov)
+    Y[, k] <- t(sum(ss))
+  }
+  Y = sqrt(Y / n)
+  t = t(round(n / 2 + (0:nrow(Y) - 1) * (n - nov)))
+  return(Y)
+}
