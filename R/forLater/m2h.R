343ed276d47741b6e9fd42043e0cf735278c0271
dasweeney4423 <dasweeney4423@gmail.com>
2017-06-15 11:44:41 -0400

    translated m2h into R code

---
+++ R/forLater/m2h.R
@@ -0,0 +1,63 @@
+#' Compute heading, field intensity and inclination angle by gimballingthe magnetic field measurement matrix with the pitch and roll estimated from the accelerometer matrix.
+#'
+#' @param M The magnetometer signal matrix, M=[mx,my,mz] in any consistent unit (e.g., in uT or Gauss).
+#' @param A An nx3 acceleration matrix with columns [ax ay az]. Acceleration canbe in any consistent unit, e.g., g or m/s^2. 
+#' @param fc (optional) The cut-off frequency of a low-pass filter to apply to A and M before computing heading. The filter cut-off frequency is with respect to 1=Nyquist frequency. Filtering adds no group delay. If fc is not specified, no filtering is performed.
+#' @return h The heading in radians in the same frame as M. The heading is with respect to magnetic north (i.e., the north vector of the navigation frame) and so must be corrected for declination. 
+#' @return v The estimated magnetic field intensity in the same units as M. This is just the 2-norm of M after filtering (if specified).
+#' @return incl The estimated field inclination angle (i.e., the angle with respect to the horizontal plane) in radians. By convention, a field vector pointing below the horizon has a positive inclination angle. See note in the function if using incl.
+#' @note Output sampling rate is the same as the input sampling rate, i.e., h, v, and incl are estimated with the same sampling rate as M and A and so are each nx1 vectors.
+#' @note Frame: This function assumes a [north,east,up] navigation frame and a [forward,right,up] local frame. North and east are magnetic, not true. In these frames a positive heading is a clockwise rotation around the z-axis. 
+#' @note The heading is computed with respect to the frame of M and is the magnetic heading NOT the true heading. M and A must have the same sampling rate, frame, and number of rows.
+#' @example list(h = h, v = v, incl = incl) <- m2h(M = matrix(c(22, -24, 14), nrow = 1), 
+#'                                                 A = matrix(c(-0.3, 0.52, 0.8), nrow = 1), fc = NULL)
+#' #Returns: h=0.89486 radians, v=34.117, incl=0.20181 radians.
+
+m2h <- function(M, A, fc = NULL) {
+  if (nargs() < 2) {
+    stop("inputs for both M and A are required")
+  }
+  if (nrow(M) * ncol(M) == 3) {
+    M <- t(M)
+  }
+  if (nrow(A) * ncol(A) == 3) {
+    A <- t(A)
+  }
+  if (nrow(A) != nrow(M)) {
+    stop("A and M must have the same number of rows/n")
+  }
+  if (!is.null(fc)) {
+    if (nrow(A) > (8/fc)) {
+      M <- fir_nodelay(M, round(8/fc), fc)
+      A <- fir_nodelay(A, round(8/fc), fc)
+    }
+  }
+  #get the pitch and roll from A
+  listpr <- a2pr(A)
+  p <- listpr$p
+  r <-listpr$r
+  cp <- cos(p)
+  sp <- sin(p)
+  cr <- cos(r)
+  sr <- sin(r)
+  Tx <- c(cp, (-sr * sp), (-cr * sp))
+  Ty <- c(rep(0, length(cp)), cr, -sr)
+  Tz <- c(sp, (sr * cp), (cr * cp))
+  Mh <- cbind(rowSums(M * Tx), rowSums(M * Ty), rowSums(M * Tz))
+  #heading estimate in FRU system
+  h  <- atan2(-Mh[, 2], Mh[, 1])
+  #compute mag field intensity and inclination
+  v <- sqrt(rowSums(M^2))
+  #compute inclination
+  suppressWarnings(x <- asin(Mh[, 3] / v))
+  signvector <- Mh[, 3] / v
+  for(i in 1: length(x)){
+    if(is.nan(x[i])){
+      x[i]<-asin(1) * sign(signvector[i])
+    }
+  }
+  incl <- -x
+  #Mh[, ] is (sp * Mx + srcp * My + crcp * Mz) which is A.M if there is no specific acceleration. So the inclination angle computed here is the same as the angle computed directly from A and M by the function inclination.R if there is no specific acceleration. If there is specific acceleration, both methods produce inclination angle estimates with errors and the errors are different because of the different computational methods.
+  return(list(h = h, v = v, incl = incl))
+}
+    
