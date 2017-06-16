ff90dd5c8189373a0e77b98fa9baef995ddd292e
dasweeney4423 <dasweeney4423@gmail.com>
2017-06-16 13:08:33 -0400

    script for enhanced fir_nodelay to better match matlab

---
+++ R/forLater/fir_nodelay2.R
@@ -0,0 +1,16 @@
+fir_nodelay <- function(x,n,fp,qual) {
+  n = floor(n/2)*2
+  if (nargs() == 4) {
+    h = signal::fir1(n,fp,qual)
+  } else {
+    h = signal::fir1(n,fp)
+  }
+  if (nrow(x) == 1) {
+    x <- t(x)
+  }
+  noffs = floor(n/2)
+  y <- signal::filter(h,1,x=rbind((x[seq(noffs, 2, -1), ]), x, x[nrow(x) + seq(-1, -noffs, -1), ]))
+  y <- matrix(y, byrow = FALSE, ncol=3)
+  y <- y[n - 1 + c(1:nrow(x)), ]
+  return(y)
+}
