31a9a3220a7b2e2c56e14a28b9340ff584ff0153
dasweeney4423 <dasweeney4423@gmail.com>
2017-06-21 13:03:39 -0400

    in progress

--- R/forLater/fir_nodelay2.R
+++ R/forLater/fir_nodelay2.R
@@ -5,12 +5,26 @@ fir_nodelay <- function(x,n,fp,qual) {
   } else {
     h = signal::fir1(n,fp)
   }
+  isavector <- FALSE
   if (is.vector(x)) {
-    x <- as.matrix(x)
+    x <- t(as.matrix(x))
+    isavector <- TRUE
   }
   noffs = floor(n/2)
-  y <- signal::filter(h,1,x=rbind((x[seq(noffs, 2, -1), ]), x, x[nrow(x) + seq(-1, -noffs, -1), ]))
+  if(isavector){
+    y <- signal::filter(h,1,x=rbind((x[seq(noffs, 2, -1) ]), x, x[nrow(x) + seq(-1, -noffs, -1) ]))
+  }
+  else{
+    y <- signal::filter(h,1,x=rbind((x[seq(noffs, 2, -1), ]), x, x[nrow(x) + seq(-1, -noffs, -1), ]))
+  }
+  #y <- signal::filter(h,1,x=rbind((x[seq(noffs, 2, -1), ]), x, x[nrow(x) + seq(-1, -noffs, -1), ]))
   y <- matrix(y, byrow = FALSE, ncol=3)
+  #if(isavector){
+  # y <- y[n - 1 + c(1:nrow(x)) ]
+  #}
+  #else{
+  #  y <- y[n - 1 + c(1:nrow(x)), ]
+  #}
   y <- y[n - 1 + c(1:nrow(x)), ]
   return(y)
 }
