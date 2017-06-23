4249938bb303207312de8bc4d986d32455e855a8
dasweeney4423 <dasweeney4423@gmail.com>
2017-06-23 12:16:53 -0400

    started translating block_acf to matlab

---
+++ matlab/R2M/block_acf.m
@@ -0,0 +1,57 @@
+function [block_acf] = block_acf(resids, blocks, max_lag, make_plot)
+% Compute autocorrelation function, respecting grouping by a categorical variable
+%   
+% This function allows calculation of an ACF for a dataset with multiple 
+%    independent units (for example, data from several individuals, data 
+%    from multiple dives by an individual animal, etc.). The groups 
+%    (individual, dive, etc.) should be coded in a categorical variable. 
+%    The function calculates correlation coefficients over all levels of 
+%    the categorical variable, but respecting divisions between levels 
+%    (for example, individual animals are kept separate).
+%
+% Inputs:
+%    resids: The variable for which the ACF is to be computed (often a 
+%            vector of residuals from a fitted model)
+%    blocks: A categorical variable indicating the groupings (must be the 
+%            same length as resids. ACF will be computed only for data 
+%            points within the same block.)
+%    max_lag: ACF will be computed at 0-max_lag lags, ignoring all 
+%             observations that span blocks. Defaults to the minimum number 
+%             of observations in any block. The function will allow you to 
+%             specify a max_lag longer than the shortest block if you so choose.
+%   make_plot: Logical. Should a plot be produced? Defaults to TRUE.
+
+blocks <- as.factor(blocks)
+
+if length(blocks) ~= length(resids)
+    warning("blocks and resids must be the same length.")
+end
+
+if ismissing(max_lag)
+    max_lag = min(tapply(blocks, blocks, length))
+end
+    
+%get indices of last element of each block (excluding the last block)
+i1 = cumsum(as.vector(utils::head(tapply(blocks, blocks, length), -1)))
+r = resids
+block_acf = ones(max_lag + 1, 1)
+
+for k = 1:max_lag
+    %%%%%%%%%%%%%%%%%%%%%%%%for (b in 1:length(i1)) {
+        %%%%%%%%%%%%%%%%%%%%r <- append(r, NA, i1[b])
+    %%%%%%%%%%%%%%%%%%%%%%%%end
+    %adjust for the growing r
+    %%%%%%%%%%%%%%%%%%%%%%%%i1 = i1 + utils::head(c(0:(-1 + nlevels(blocks))), -1)
+    %%%%%%%%%%%%%%%%%%%%%%%%rmmissing(r)
+    this_acf = autocorr(r, max_lag)
+    block_acf(k + 1) = this_acf(k + 1, 1, 1)
+end
+
+if ismissing(make_plot) || make_plot == TRUE
+    %get an acf object in which the block_acf results will be inserted. Facilitates plotting.
+    A = autocorr(resids, max_lag)
+    %insert coefficients from block_acf into A
+    A(:, 1, 1] = block_acf
+    %plot block_acf
+    plot(A)
+end
