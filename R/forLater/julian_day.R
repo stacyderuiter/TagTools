38673ab3b6a80693732a670263236f49dfa84adc
dasweeney4423 <dasweeney4423@gmail.com>
2017-07-11 13:35:49 -0400

    working on julian_day

--- R/forLater/julian_day.R
+++ R/forLater/julian_day.R
@@ -1,12 +1,13 @@
 #' Convert between dates and Julian day numbers.
 #' 
-#' @description (n <- julianday) returns the Julian day number for today. (n = julianday(y,d)) where y is a single year or a vector of years and d is a single day number or a vector of daynumbers, returns the date vector [year,month,day] for each year,day pair. (n = julianday(y,m,d)) where y is a single year or a vector of years, m is a single month or vector of months, and d is a single month day or a vector of month days, returns the Julian day number for each year, month, day.
+#' @description (n <- julianday) returns the Julian day number for today. (n = julianday(y,d)) where y is a single year or a vector of years and d is a single day number or a vector of daynumbers, returns the date vector [year,month,day] for each year, day pair. (n = julianday(y,m,d)) where y is a single year or a vector of years, m is a single month or vector of months, and d is a single month day or a vector of month days, returns the Julian day number for each year, month, day.
 #' @param y A single year or vector of years
 #' @param d A single day or vector of days
 #' @param m A single month or vector of months
 #' @return n See the description section for details on the return.
+#' @export
 #' @example julian_day(y = 2016, d = 12, m =10) #Returns: 286
-#'          julian_day(y = 2016, d =286) #Returns: c(2016, 10, 12)
+#'          julian_day(y = 2016, d = 286) #Returns: "2016-10-12"
 
 julian_day <- function(y = NULL, d = NULL, m = NULL) {
   if (nargs() == 0) {
@@ -34,7 +35,8 @@ julian_day <- function(y = NULL, d = NULL, m = NULL) {
   if (length(d) < k) {
     d[length(d) + 1:k] <- d[length(d)]
   }
-  n <- floor(as.numeric(julian(ISOdate(y, m, d))) - 16800)
+  t <- as.POSIXlt(as.Date(ISOdate(y, m, d)))
+  n <- t$yday + 1
   for (j in 1:length(n)) {
     if (n[j] > 365) {
       n[j] <- n[j] - 366
