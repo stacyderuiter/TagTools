128ecec197249451df7d1a0f98eef8012440aa03
dasweeney4423 <dasweeney4423@gmail.com>
2017-07-13 10:25:42 -0400

    finished composing PrCA from seal paper

--- R/forLater/David_project_testing/PrCA.R
+++ R/forLater/David_project_testing/PrCA.R
@@ -1,9 +1,25 @@
-PrCA <- function(A, fs, thresV){
+#' Automated detection of prey catch attempts (PrCA) from triaxial acceleration data from seals.
+#' 
+#' @param A The acceleration matrix with columns [ax ay az]. Acceleration must be in m/s^2. 
+#' @param fs The sampling rate in Hz of the acceleration signals.
+#' @param thresV (optional) A user selectable threshold in the same units as A which is used in the process of checking for prey catpure attempts from the equation varS >= varA + thresV at a given second in time. varS is the change in magA (magnitute in acceleration) over one second of time. varA is the per second running average of change in acceleration. Default level as specified by the source is 5 m/s^2.
+#' @return captures A list containing vectors for the capture times (seconds since start of data recording) and capture varS (change in magA over one second of time) values.
+#' @note Source: Cox, S. L., Orgeret, F., Gesta, M., Rodde, C., Heizer, I., Weimerskirch, H. and Guinet, C. (), Processing of acceleration and dive data on-board satellite relay tags to investigate diving and foraging behaviour in free-ranging marine predators. Methods Ecol Evol. Accepted Author Manuscript. doi:10.1111/2041-210X.12845 
+
+PrCA <- function(A, fs, thresV) {
+  if (missing(fs)) {
+    stop("Inputs for A and fs are both required")
+  }
+  if (missing(thresV)) {
+    thresV <- 5
+  }
+  
   #calculate magnitute in acceleration
   magA <- matrix(0, nrow(A), 1)
   for (i in 1:nrow(A)) {
     magA[i] <- sqrt(A[i, 1]^2 + A[i, 2]^2 + A[i, 3]^2)
   }
+  
   #calculate the change in magA over one second of time
   var <- buffer_no_delay(magA, fs, 0)
   varS_col <- matrix(0,(nrow(var) - 1), ncol(var))
@@ -12,6 +28,7 @@ PrCA <- function(A, fs, thresV){
     varS_col[, j] <- abs(diff(var[, j]))
     varS[j] <- sum(varS_col[, j])
   }
+  
   #calculate per second running average of change in acceleration
   ma <- function(x, n) {
     ans <- matrix(0,1, ncol(varS))
@@ -31,4 +48,15 @@ PrCA <- function(A, fs, thresV){
     return(ans)
   }
   varA <- ma(varS, 11)
+  
+  #find prey capture attempts
+  cap <- which(varS >= (varA + thresV))
+  vars <- matrix(c(1:ncol(varS), varS), nrow = 2, byrow = TRUE)
+  captures_times <- vars[1, cap]
+  captures_varS <- varS[cap]
+  
+  #create a list of capture times and capture varS values
+  captures <- list(captures_times = captures_times, captures_varS = captures_varS)
+  
+  return(captures)
 }
--- matlab/David_project_testing/PrCA.m
+++ matlab/David_project_testing/PrCA.m
@@ -1,7 +1,36 @@
 function captures = PrCA(A, fs, thresV)
+% Automated detection of prey catch attempts (PrCA) from triaxial
+%   acceleration data from seals.
+%
+% INPUTS:
+%   A = The acceleration matrix with columns [ax ay az]. Acceleration must
+%       be in m/s^2.
+%   fs = The sampling rate in Hz of the acceleration signals.
+%   thresV = A user selectable threshold in the same units as A which is
+%       used in the process of checking for prey catpure attempts from the
+%       equation varS >= varA + thresV at a given second in time. varS is
+%       the change in magA (magnitute in acceleration) over one second of
+%       time. varA is the per second running average of change in
+%       acceleration. Default level as specified by the source is 5 m/s^2.
+%
+% OUTPUTS:
+%   captures =  A structure containing vectors for the capture times 
+%       (seconds since start of data recording) and capture varS (change in
+%       magA over one second of time) values.
+%
+% Source: 
+%   Cox, S. L., Orgeret, F., Gesta, M., Rodde, C., Heizer, I., 
+%       Weimerskirch, H. and Guinet, C. (), Processing of acceleration and 
+%       dive data on-board satellite relay tags to investigate diving and 
+%       foraging behaviour in free-ranging marine predators. Methods Ecol 
+%       Evol. Accepted Author Manuscript. doi:10.1111/2041-210X.12845 
 
 if nargin < 2
     help PrCA
+end
+
+if nargin < 3 || isempty(thresV)
+    thresV = 5;
 end
 
 %calculate magnitute in acceleration
@@ -11,7 +40,7 @@ for i = 1:size(A, 1)
 end
 
 %calculate the change in magA over one second of time
-var = buffer(magA, fs, 0, 'nodelay');
+[var, ~] = buffer(magA, fs, 0, 'nodelay');
 for j = 1:size(var, 2)
     varS_col(:, j) = abs(diff(var(:, j)));
     varS(j) = sum(varS_col(:, j));
@@ -22,5 +51,13 @@ varA = movmean(varS, 11);
 
 %find prey capture attempts
 cap = varS >= (varA + thresV);
-cap_varS = varS(cap);
+vars = [1:size(varS, 2); varS];
+captures_times = vars(1, cap);
+captures_varS = varS(cap);
+
+%create structure of prey capture times and their respective varS values
+field1 = 'captures_times';  value1 = captures_times;
+field2 = 'captures_varS';  value2 = captures_varS;
+captures = struct(field1,value1,field2,value2);
+
 end
