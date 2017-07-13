7d5c714d1243b04b5b3a229c9c15a4b0aebae3df
dasweeney4423 <dasweeney4423@gmail.com>
2017-07-13 11:47:47 -0400

    almost done with plotting

--- R/forLater/David_project_testing/find_peaks.R
+++ R/forLater/David_project_testing/find_peaks.R
@@ -1,20 +1,20 @@
 #' This function detects peaks in jerk data that exceed a specfied threshold and returns each peak's start time, end time, maximum jerk value, and time of the maximum jerk.
 #' 
-#' @param A The acceleration matrix with columns [ax ay az]. Acceleration can be in any consistent unit (e.g. g or m/s^2). This is used to calculate the jerk using njerk().
+#' @param A The acceleration matrix with columns [ax ay az]. Acceleration can be in any consistent unit (e.g. g or m/s^2). This is used to calculate the jerk using n_jerk().
 #' @param fs The sampling rate in Hz of the acceleration signals. This is used to calculate the bktime in the case that the input for bktime is missing.
 #' @param thresh The threshold level above which peaks in the jerk signal are detected. If the input for thresh is missing/empty, the default level is the 0.99 quantile 
 #' @param bktime The specified length of time between jerk values detected above the threshold value that is required for each value to be considered a separate and unique peak. If the input for bktime is missing/empty, the default level for bktime is 5 times the sampling rate (fs). This is equivalent to 5 seconds of time.
-#' @param plot A conditional input. If the input is TRUE or missing, an interactive plot is generated, allowing the user to manipulate the thresh and bktime values and observe the changes in peak detection. If the input is FALSE, the interactive plot is not generated.
+#' @param plot_jerk A conditional input. If the input is TRUE or missing, an interactive plot is generated, allowing the user to manipulate the thresh and bktime values and observe the changes in peak detection. If the input is FALSE, the interactive plot is not generated.
 #' @return peaks A list containing vectors for the start times, end times, peak times, and peak maxima. All times are presented as the sampling value. Peak maxima are presented in the same units as A. If A is in m/s^2, the peak maxima have units of m/s^3. If the units of A are in g, the peak maxima have unit g/s.
 #' @note As specified above under the description for the input of plot, an interactive plot can be generated, allowing the user to manipulate the thresh and bktime values and observe the changes in peak detection. The plot output is only given if the input for plot is specified as true or if the input is left missing/empty.
 
-find_peaks <- function(A, fs, thresh = NULL, bktime = NULL, plot = NULL) {
+find_peaks <- function(A, fs, thresh = NULL, bktime = NULL, plot_jerk = NULL) {
   if (missing(A) | missing(fs)) {
     stop("inputs for A and fs are both required")
   }
   
   #calculate jerk of A
-  j <- njerk(A, fs)
+  j <- n_jerk(A, fs)
   
   if (is.null(thresh) == TRUE) {
     thresh <- stats::quantile(j, c(0.99))
@@ -22,8 +22,8 @@ find_peaks <- function(A, fs, thresh = NULL, bktime = NULL, plot = NULL) {
   if (is.null(bktime) == TRUE) {
     bktime <- 5 * fs
   }
-  if (is.null(plot) == TRUE) {
-    plot <- TRUE
+  if (is.null(plot_jerk) == TRUE) {
+    plot_jerk <- TRUE
   }
   
   #create matrix for jerk and corresponding sampling number
@@ -64,11 +64,20 @@ find_peaks <- function(A, fs, thresh = NULL, bktime = NULL, plot = NULL) {
   peaks <- list(start_time = start_time, end_time = end_time, peak_time = peak_time, peak_max = peak_max)
   
   
-  if (plot == TRUE) {
+  if (plot_jerk == TRUE) {
     #create a plot which allows for the thresh and bktime to be manipulated
-    manipulate::manipulate(plot(j),
-                           thresh = manipulate::slider(.99, 1, initial = .99, label = thresh, step = .0001, ticks = TRUE), 
-                           bktime = manipulate::slider((1 / fs * fs), (fs * fs), step = 1, label = bktime, ticks = TRUE))
+    plot(j, type = "l", col = "blue")
+    #points(peaks$peak_time, pch = 9, type = "l", col = "orange", cex = 20)
+    pts <- graphics::locator(n = 3)
+    thresh <- pts$y[1]
+    bktime <- pts$x[3] - pts$x[2]
+    peaks <- find_peaks(A, fs, thresh, bktime, plot_jerk = FALSE)
+    #manipulate::manipulate(plot(j),
+    #                       thresh = manipulate::slider(.99, 1, initial = .99, label = thresh, step = .0001, ticks = TRUE), 
+    #                       bktime = manipulate::slider((1 / fs * fs), (fs * fs), step = 1, label = bktime, ticks = TRUE))
+  } else {
+    plot(j, type = "l", col = "blue")
+    #points(peaks$peak_time, pch = 9, type = "l", col = "orange", cex = 2)
   }
   
   return(peaks)
