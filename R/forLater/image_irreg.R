644426c841a3788aa456caae5382ab0d3b8268cb
dasweeney4423 <dasweeney4423@gmail.com>
2017-07-14 11:31:40 -0400

    finished image_irreg

--- R/forLater/image_irreg.R
+++ R/forLater/image_irreg.R
@@ -17,11 +17,11 @@ image_irreg <- function(x, y, R) {
   X <- matrix(c(0, 0, 1, 1), ncol = 1) %*% xdiff + matrix(1, 4, 1) %*% x
   Y <- matrix(c(0, 1, 1, 0), ncol = 1) %*% matrix(1, 1, length(x)) 
   ydiff <- c(diff(y), y[length(y)] - y[length(y) - 1])
+  graphics::plot(NA,xlim = c(min(X[,]), max(X[,])), ylim = c(min(ydiff[1] * Y[,  which(!is.na(R[, 1]))] + y[1]), max(ydiff[length(y)] * Y[, which(!is.na(R[, length(y)]))] + y[length(y)])))
   for (k in 1:length(y)) {
     zk <- which(!is.na(R[, k]))
-    xx <- X[, zk]
-    yy <- (ydiff[k] * Y[, zk] + y[k])
-    cols <- R[zk, k]
-    plot3D::polygon2D(x = xx, y = yy, col = cols)
+    polygon(x =X[, zk], y = (ydiff[k] * Y[, zk] + y[k]), col = R[zk,k], lty = 0)
+    #plot3D::polygon2D(x = X[, zk], y = (ydiff[k] * Y[, zk] + y[k]), col = R[zk, k])
+    
   }
 }
