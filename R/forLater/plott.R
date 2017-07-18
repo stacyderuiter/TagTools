1b8ba723198077c70b4b243642a525c5b5678c8d
dasweeney4423 <dasweeney4423@gmail.com>
2017-07-18 11:43:14 -0400

    working on plott

--- R/forLater/plott.R
+++ R/forLater/plott.R
@@ -1,10 +1,9 @@
 #' Plot sensor time series against time in a single or multi-paneled figure with linked x-axes. This is useful for comparing measurements across different sensors. The time axis is automatically displayed in seconds, minutes, hours, or days according to the span of the data.
 #' 
-#' @param X, Y, etc, Lists or vectors/matrices of time series data.
-#' @param fsx, fsy, etc, The sampling rates in Hz for each data object. Sampling rates are not needed when the data object is a list.
+#' @param X X, Y, etc, Lists or vectors/matrices of time series data.
+#' @param fsx fsx, fsy, etc, The sampling rates in Hz for each data object. Sampling rates are not needed when the data object is a list.
 #' @param r (optional) Can be used reverse the direction of the y-axis for the data object that it follows if r='r'. This is useful for plotting dive profiles which match the physical situation i.e., with greater depths lower in the display. If r is a number, it specifies the number of seconds time offset for the preceding data object. A positive value means that these data were collected later than the other objects and so should be plotted more to the right-hand side.
 #' @return ax A vector of handles to the axes created.
-#' @return h is a list of vectors of handles to the lines plotted. There is a list of handles for each axis.
 #' @note This is a flexible plotting tool which can be used to display and explore sensor data with different sampling rates on a uniform time grid. Zooming any of the panels should cause all of the panels to zoom in or out to match the x-axis.
 
 plott <- function(...) {
@@ -20,26 +19,14 @@ plott <- function(...) {
   args_container <- as.list(match.call())
   fsrt <- matrix(0, length(args_container), 4) 
   X <- list()
-<<<<<<< HEAD
-  for (k in 1:length(args_container)) {
+  for (k in 2:length(args_container)) {
     x <- args_container[[k]] 
     if (is.list(x)) {
-      #this input is a sensor structure
-      if (is.null(x[["fs"]]) & is.null(x[["data"]])) {
-        X[[length(x) + 1]] <- x$data
-        fs[length(X), 1] <- x$fs
-      } else {
-=======
-  for (k in 2:length(args_container)){
-    x <- args_container[[k]] 
-    if (is.list(x)){
       # this input is a sensor structure
-      if(!is.null(x[['fs']]) && !is.null(x[['data']])){
+      if(!is.null(x[['fs']]) && !is.null(x[['data']])) {
         X[[length(x)+1]] <- x$data
         fs[length(X),1] <- x$fs
-      }
-      else{
->>>>>>> 5b51228cd39992c6fa5f756beda95ca05c763985
+      } else {
         stop("sensor structure must have data and fs fields!")
       }
     } else {
@@ -78,7 +65,7 @@ plott <- function(...) {
   h <- list()
   for (k in 1:length(X)) { #now we are ready to plot
     axis(ax[k]) 
-    h[[k]] <- plot(((1:nrow(X[[k]])) / fsrt[k, 1] + fsrt[k, 3]) * (1 / ddiv), X[[k]])
+    plot(((1:nrow(X[[k]])) / fsrt[k, 1] + fsrt[k, 3]) * (1 / ddiv), X[[k]])
     set(ax[k], 'XLim', xlims)
     if (fsrt[k, 2] == 1) {
       set(ax[k], 'YDir', 'reverse')
@@ -87,5 +74,5 @@ plott <- function(...) {
   xlab <- sprintf('Time (%s)', L[[divk]])
   xlabel(ax[length(ax)], xlab) 
   linkaxes(ax, 'x')
-  return(ax, h)
+  return(ax)
 }
