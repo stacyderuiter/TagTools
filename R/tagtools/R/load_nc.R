3ba9a58b6bae66b90f00696597644ce8ac0a6773
Stacy DeRuiter <stacy@calvin.edu>
2017-07-20 13:51:04 -0400

    rearrange order of items in sensor data list in tagtools objects

--- R/tagtools/R/load_nc.R
+++ R/tagtools/R/load_nc.R
@@ -2,14 +2,18 @@
 #' 
 #' @description This function loads a tag dataset from a netCDF file (this is an archival file format supported by the tagtools package and suitable for submission to online data archives).
 #' @param file File name (and path, if necessary) of netCDF file to be read, as a quoted character string.
+#' @param (Optional) A list of quoted character strings giving the exact names of variables to be read in. Default is to read all variables present in the file. parameters should be read in.
 #' @result X, if specified, is a structure containing sensor and metadata structures. The field names in X will be the same as the names of the variables in the NetCDF file, e.g., if the file contains A and P, X will have fields X$A, X$P and X$info (the file metadata).
 #' @example \dontrun{load_nc('testset1.nc')}
 #' 
 
- load_nc <- function(file){
+ load_nc <- function(file, which_vars=NULL){
    file_conn <- ncdf4::nc_open(file)
    #get variable names present in this file
    vars <- names(file_conn$var)
+   if (!is.null(which_vars)){
+     vars <- vars[vars %in% which_vars]
+   }
    #read in the variables one by one and store in a list
    X <- list()
    for (v in 1:length(vars)){
@@ -17,8 +21,11 @@
      X[[v]] <- ncdf4::ncatt_get(file_conn, vars[v])
      # remove redundant name label
      X[[v]]$name <- NULL
-     #add the actual data matrix or vector
+     field_names <- names(X[[v]])
+     # add the actual data matrix or vector
      X[[v]]$data <- ncdf4::ncvar_get(file_conn, vars[v])
+     # make sure the sensor data is the first element of X[[v]]
+     X[[v]] <- X[[v]][c('data', field_names)]
    }
    # entries of X should match variable names from netCDF file
    names(X) <- vars
