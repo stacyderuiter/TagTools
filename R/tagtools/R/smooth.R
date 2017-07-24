b9e2e72bef0817ff74d50511addb6a169a8a4322
dasweeney4423 <dasweeney4423@gmail.com>
2017-07-24 14:43:41 -0400

    edit of example

--- R/tagtools/R/smooth.R
+++ R/tagtools/R/smooth.R
@@ -4,8 +4,8 @@
 #' @param n The smoothing parameter - use a larger number to smooth more. n must be greater than 1. Signal components above 1/n of the Nyquist frequency are filtered out.
 #' @return y The input signal has the first and fifth harmonic. Applying the low-pass filter removes most of the fifth harmonic so the output appears as a sinewave except for the first few samples which are affected by the filter startup transient. Smooth uses fir_nodelay to perform the filtering and so introduces no delay.
 #' @export
-#' @example x <- matrix(c(1, -0.5, 0.1, 0.8, -0.2, 0.6, 0.5, -0.9, -0.7), byrow = TRUE, nrow = 3)
-#'          y <- smooth(x, n = 2)
+#' @example x <- sin((2*pi*0.05)%*%t(c(1:100)))+cos((2*pi*0.25)%*%t(c(1:100)))
+#'          y <- smooth(x, n = 4)
 
 smooth <- function(x, n) {
     y <- vector(mode = "numeric", length = 0)
