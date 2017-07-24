b2c849ecce194f623786b366527ed21c4e496036
dasweeney4423 <dasweeney4423@gmail.com>
2017-07-24 16:33:19 -0400

    fixed typo

--- R/tagtools/R/cst2hms.R
+++ R/tagtools/R/cst2hms.R
@@ -4,12 +4,12 @@
 #' @param tag The tag id string (eg "zc11_267a")
 #' @param cst A scalar or a vector of times to convert (in seconds since start of recording)
 #' @param d3 Give the value 0 if the tag was a dtag2, or 1 if a d3 (if 1, then it will be assumed that the TAGON in the cal file is in UTC and GMT2LOC will be used to covert to local).
-#' @param TAGON (optional) A vector with tagon time as [yyyy mm dd hh mm ss], in case there is no cal file for this tag avail.
+#' @param TAGON (optional) A vector with tagon time as [yyyy mm dd hh mm ss], in case there is no cal file for this tag available.
 #' @param GMT2LOC (optional) The conversion factor for GMT to local time, if d3=1 and tagon is given then GMT2LOC should be given as well.
 #' @return hms The output for a date/time string which is a matrix of strings where row n is a string indicating the local date and time for entry n of csts.
 #' @export
 
-cst2hms <- function(tag,cst, d3, TAGON, GMT2LOC) {
+cst2hms <- function(tag, cst, d3, TAGON, GMT2LOC) {
   if (d3 == 1) {
     TAGON[4] = TAGON[4] + GMT2LOC #convert GMT tagon time to local time
   }
