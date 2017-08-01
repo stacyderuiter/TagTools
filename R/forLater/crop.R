e80e8ba8abe6409fb98f31a01d877854c6fe0394
dasweeney4423 <dasweeney4423@gmail.com>
2017-08-01 15:28:35 -0400

    finished and checked crop series of functions

--- R/forLater/crop.R
+++ R/forLater/crop.R
@@ -11,70 +11,89 @@
 #'  \item{\strong{T: }} A vector of sampling times for Y. This is only returned if X is irregularly sampled and X is not a sensor list. If X is a sensor list, the sampling times are stored in the list.
 #'  \item{\strong{tcues: }} tcues is a two-element vector containing the start and end time cue in seconds of the data segment kept, i.e., tcues = c(start_time, end_time).
 #' }
-#' @example load_nc('testset3')
-#'          Pc <- crop(P)		#interactively select a section of data
-#'          plott(Pc)
+#' @example data <- load_nc('testset3.nc')
+#'          Pc <- crop(data$P)		#interactively select a section of data
+#'          Ydata <- list(depth = Pc$Y)
+#'          plott(Ydata)
 #'          #plot shows the cropped section
 
-crop <- function(X, sampling_rate) {
+crop <- function(X, sampling_rate = NULL) {
   if (missing(X)) {
     stop("X is a required input")
   }
   if (is.list(X)) {
-    x <- list$data
+    x <- X$data
     sampling_rate <- X$sampling_rate
     if (is.null(x)) {
       stop("data cannot be empty")
     }
+    if (!is.matrix(x)) {
+      x <- matrix(x, ncol = 1)
+    }
   } else {
-    if (missing(sampling_rate)) {
+    if (is.null(sampling_rate)) {
       stop("inputs for X and sampling_rate are required")
     }
     x <- X
-    if (is.vector(x) | nrow(x) == 1) {
+    if (!is.matrix(x)) {
+      x <- matrix(x, ncol = 1)
+    }
+    if (nrow(x) == 1) {
       x <- matrix(x, nrow = length(x), ncol = 1)
     }
   }
   graphics.off()
   if (length(sampling_rate) > 1) {
-    plot(sampling_rate, x, type = "p", xlab = "Time (seconds)")
+    matplot(sampling_rate, x, type = "p", xlab = "Time (seconds)")
+    matplot(sampling_rate, x, type = "p", xlab = "Time (seconds)", ylim = c(floor(par("usr")[3]), ceiling(par("usr")[4])))
   } else {
-    plot((c(1:nrow(x)) / sampling_rate), x, type = "l", xlab = "Time (seconds)")
+    matplot((c(1:nrow(x)) / sampling_rate), x, type = "l", xlab = "Time (seconds)")
+    matplot((c(1:nrow(x)) / sampling_rate), x, type = "l", xlab = "Time (seconds)", ylim = c(floor(par("usr")[3]), ceiling(par("usr")[4])))
   }
-  print("Position your cursor and then click once followed by clicking FINISH to change the start, or click twice in the same spot followed by clicking FINISH to change the end")
+  print("Position your cursor and then click once followed by clicking FINISH to change the start, or click twice in the same spot followed by clicking FINISH to change the end. If you wish to change both the start and end click once at the start time desired and twice at the end time desired.")
   if (length(sampling_rate) > 1) {
     tcues <- c(min(sampling_rate), max(sampling_rate))
   } else {
     tcues <- c(0, (nrow(x) / sampling_rate))
   }
   LIMS <- tcues
-  lines(matrix((c(1,1) * tcues[1]), ncol = 1), col = "green", lwd = 1.5)
-  points(tcues[1], mean((par("usr")[3]+.04), (par("usr")[4]-.04)), col = "green", pch = 6, cex = 12)
-  lines(matrix((c(1,1) * tcues[2]), ncol = 1), col = "red", lwd = 1.5)
-  points(tcues[2], mean((par("usr")[3]+.04), (par("usr")[4]-.04)), col = "red", pch = 6, cex = 12)
-  pts <- graphics::locator(2)
+  pts <- graphics::locator(3)   #This should probably be a shiny app at some point in order to make the interactiveness more user friendly
   if (length(pts$x) == 1) {
     t <- pts$x
     tcues[1] <- max(LIMS[1], min(t, tcues[2]))
-    lines(matrix((c(1,1) * tcues[1]), nrow = 1), col = "green", lwd = 1.5)
-    points(tcues[1], mean((par("usr")[3]+.04), (par("usr")[4]-.04)), col = "green", pch = 6, cex = 12)
-    lines(matrix((c(1,1) * tcues[2]), ncol = 1), col = "red", lwd = 1.5)
-    points(tcues[2], mean((par("usr")[3]+.04), (par("usr")[4]-.04)), col = "red", pch = 6, cex = 12)
+    abline(v = (c(1,1) * tcues[1]), col = "green", lwd = 1.5)
+    points(tcues[1], mean(c((par("usr")[3]), (par("usr")[4]))), col = "green", pch = 6)
+    abline(v = (c(1,1) * tcues[2]),  col = "red", lwd = 1.5)
+    points(tcues[2], mean(c((par("usr")[3]), (par("usr")[4]))), col = "red", pch = 6)
   } else {
-    t < pts$x[2]
-    tcues[2] <- max(LIMS[2], min(t, tcues[1]))
-    lines(matrix((c(1,1) * tcues[1]), ncol = 1), col = "green", lwd = 1.5)
-    points(tcues[1], mean((par("usr")[3]+.04), (par("usr")[4]-.04)), col = "green", pch = 6, cex = 12)
-    lines(matrix((c(1,1) * tcues[2]), nrow = 1), col = "red", lwd = 1.5)
-    points(tcues[2], mean((par("usr")[3]+.04), (par("usr")[4]-.04)), col = "red", pch = 6, cex = 12)
+    if (length(pts$x) == 2) {
+      t <- pts$x[2]
+      tcues[2] <- min(LIMS[2], max(t, tcues[1]))
+      abline(v = (c(1,1) * tcues[1]), col = "green", lwd = 1.5)
+      points(tcues[1], mean(c((par("usr")[3]), (par("usr")[4]))), col = "green", pch = 6)
+      abline(v = (c(1,1) * tcues[2]), col = "red", lwd = 1.5)
+      points(tcues[2], mean(c((par("usr")[3]), (par("usr")[4]))), col = "red", pch = 6)
+    } else {
+      if (length(pts$x) == 3) {
+        ts <- pts$x[1]
+        tcues[1] <- max(LIMS[1], min(ts, tcues[2]))
+        abline(v = (c(1,1) * tcues[1]), col = "green", lwd = 1.5)
+        points(tcues[1], mean(c((par("usr")[3]), (par("usr")[4]))), col = "green", pch = 6)
+        te <- pts$x[3]
+        tcues[2] <- min(LIMS[2], max(te, tcues[1]))
+        abline(v = (c(1,1) * tcues[2]), col = "red", lwd = 1.5)
+        points(tcues[2], mean(c((par("usr")[3]), (par("usr")[4]))), col = "red", pch = 6)
+      }
+    }
   }
   if (is.list(X)) {
-    Y <- crop_to(X, tcues)$X
+    cto <- crop_to(X, tcues = tcues)
+    Y <- cto$X
     T <- tcues
   } else {
-    list <- crop_to(x, sampling_rate, tcues)
-    Y <- list$X
-    T <- list$T
+    cto <- crop_to(x, sampling_rate, tcues)
+    Y <- cto$X
+    T <- cto$T
     if (length(sampling_rate) == 1) {
       T <- tcues
     }
