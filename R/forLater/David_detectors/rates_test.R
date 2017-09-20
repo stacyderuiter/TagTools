3c80b30231bc1dc32bab054e284a4fd2bfab492a
dasweeney4423 <dasweeney4423@gmail.com>
2017-09-20 12:23:32 -0400

    rocplot only plots, rates_test only gives the rates

--- R/forLater/David_detectors/rates_test.R
+++ R/forLater/David_detectors/rates_test.R
@@ -1,9 +1,13 @@
-rocplot <- function(data, sampling_rate, FUN, bktime, indices, events, ntests, testint) {
+rates_test <- function(data, sampling_rate, FUN, bktime, indices, events, ntests, testint) {
   tpevents <- ((indices/sampling_rate)/bktime)
   sr <- sampling_rate
   for (k in 1:ntests) {
     if (k == 1) {
       thresh <- testint
+    } else {
+      if (k == ntests) {
+        thresh <- max(njerk(data, sampling_rate))
+      }
     }
     detections <- detect(data, sr, FUN, thresh, bktime, plot_peaks = FALSE, sampling_rate=sampling_rate)$peak_time
     True_Positive_Rate <- acc_test(detections, events, sampling_rate, tpevents)$hits_rate
@@ -16,10 +20,8 @@ rocplot <- function(data, sampling_rate, FUN, bktime, indices, events, ntests, t
     }
   }
   pts <- rbind(pts, c(1,1))
-  pts <- pts[order(pts[, 1]), ]
   True_Positive_Rate <- pts[, 1]
   False_Positive_Rate <- pts[, 2]
   rates <- cbind(True_Positive_Rate, False_Positive_Rate)
-  require(ggplot2)
-  ggplot((data.frame(rates)), aes(x = False_Positive_Rate, y = True_Positive_Rate)) + geom_point() + theme_bw() + theme(axis.text=element_text(size=15), axis.title=element_text(size=15,face="bold")) + geom_smooth(se = FALSE, span = 0.6)
+  return(rates)
 }
