fe1e06f31335bfc270e3dd44b0b98a68d14facdf
Stacy DeRuiter <stacy@calvin.edu>
2018-03-19 16:05:58 -0400

    correction to sens_struct for irreg data

--- R/tagtools/R/sens_struct.R
+++ R/tagtools/R/sens_struct.R
@@ -1,26 +1,26 @@
 #' Generate a sensor structure from a sensor data vector or matrix.
-#' 
+#'
 #' @param data sensor data vector or matrix
 #' @param depid string that provides a unique identifier for this tag deployment
 #' @param type is a string containing the first few letters of the sensor type,
 #'             e.g., acc for acceleration. These will be matched to the list of
-#'             sensor names in the sensor_names.csv file. If more than one sensor 
+#'             sensor names in the sensor_names.csv file. If more than one sensor
 #'             matches type, a warning will be given. type can be in upper or lower case.
 #' @param T (optional) is the time in seconds of each measurement in data for irregularly sampled data. The time reference (i.e., the 0 time) should be with respect to the start time of the deployment.
 #' @param fs (optional) sensor data sampling rate in Hz
 #' @param unit (optional) units in which data are sampled. Default determined by matching \code{type} with defaults in sensor_names.csv
-#' @param frame (optional) frame of reference for data axes, for example 'animal' or 'tag'. Default determined by matching \code{type} with defaults in sensor_names.csv.  
+#' @param frame (optional) frame of reference for data axes, for example 'animal' or 'tag'. Default determined by matching \code{type} with defaults in sensor_names.csv.
 #' @param name (optional) "full name" to assign to the variable. Default determined by matching \code{type} to defaults in sensor_names.csv/
 #' @param start_offset (optional) offset in start time for this sensor relative to start of tag recording. Defaults to 0.
 #' @param start_offset_units (optional) units of start_offset. default is 'second'.
 #' @return A sensor list with field \code{data} containing the data and with metadata fields pre-populated from the sensor_names.csv file. Change these manually as needed (or specify the relevant inputs to \code{sens_struct}) to the correct values.
 #' @export
-#' @examples 
+#' @examples
 #' \dontrun{
 #' #example will only work if data matrix Aw is in your workspace.
 #' #A <- sens_struct(data=Aw,fs=fs,depid='md13_134a', type='acc')}
 
-sens_struct <- function(data,fs=NULL,T=NULL, depid, type, 
+sens_struct <- function(data,fs=NULL,T=NULL, depid, type,
                         unit=NULL, frame=NULL, name=NULL,
                         start_offset=0, start_offset_units='second'){
 
@@ -30,7 +30,7 @@ sens_struct <- function(data,fs=NULL,T=NULL, depid, type,
   #sens_names <- read.csv('R/tagtools/inst/extdata/sensor_names.csv', stringsAsFactors=FALSE)
   if (missing(data) | missing(type) | missing(depid)){
     cat('Not enough inputs to sens_struct. Pre-defined sensor types are:\n',
-                   as.character(sens_names[,'name']))    
+                   as.character(sens_names[,'name']))
     stop("Please check inputs to sens_struct.")
     X = NULL ;
   }
@@ -39,15 +39,15 @@ if (length(fs)==1){# regularly sampled data
   X <- list(data = data, sampling='regular',
             sampling_rate=fs, sampling_rate_unit='Hz')
 }else{                 # irregular data
-   if (length(fs) != min(nrow(data), length(data))){
+   if (length(T) != min(nrow(data), length(data))){
       stop('number of sampling times does not match number of samples.')
    }
-  X <- list(data=matrix(0, nrow=length(fs), ncol=1+max(1,ncol(data))))
-  X$data[,1] <- fs
+  X <- list(data=matrix(0, nrow=length(T), ncol=1+max(1,ncol(data))))
+  X$data[,1] <- T
   X$data[,2:ncol(X$data)]<- data
-	X$sampling = 'irregular' 
-	X$sampling_time = 'column 1' 
-	X$sampling_time_unit = 'second' 
+	X$sampling = 'irregular'
+	X$sampling_time = 'column 1'
+	X$sampling_time_unit = 'second'
   fs = NULL
 }
 
