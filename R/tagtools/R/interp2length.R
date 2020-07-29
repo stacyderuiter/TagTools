00e37c527169902f8ea7b4ac4d68a1974d5fd6d6
Stacy DeRuiter <stacy@calvin.edu>
2020-07-29 12:32:02 -0400

    update speed_from_depth() to allow different sampling rates and minor length mismatches for p and A inputs

---
+++ R/tagtools/R/interp2length.R
@@ -0,0 +1,106 @@
+#' Interpolate regularly sampled data to increase its sampling rate and match its length to another variable.
+#' 
+#' This function is used to reduce the time span of data by cropping out any data that falls before and after two time cues.
+#' 
+#' @param X A sensor list, vector, or matrix. If x is or contains  matrix, each column is treated as an independent signal.
+#' @param Z is a sensor structure, vector or matrix whose sampling rate and length is to be matched.
+#' @param fs_in is the sampling rate in Hz of the data in X. This is only needed if X is not a sensor structure.
+#' @param fs_out is the required new sampling rate in Hz. This is only needed if Z is not given. 
+#' @param n_out is an optional length for the output data. If n_out is not given, the output data length will be the input data length * fs_out/fs_in.
+#' @return Y is a sensor structure, vector or matrix of interpolated data with the same number of columns as X.
+#' @examples 
+#'          P_dec <- decdc(harbor_seal$P, 5)
+#'          # note: you would not really want to decimate and then linearly interpolate. 
+#'          # only doing so here to create an example from existing datasets 
+#'          # that have uniform sampling rates across sensors
+#'          P_interp <- interp2length(X = P_dec, Z = harbor_seal$A)
+#' @export
+
+interp2length <- function(X, Z, fs_in = NULL, fs_out = NULL, n_out = NULL) {
+  # INPUT CHECKING ----------------------------
+  if (missing(X) | missing(Z)){
+    stop("Inputs X and Z are required for interp2length().")
+  }
+  if (is.list(X)) {
+    x <- X$data
+    fs_in <- X$sampling_rate
+  } else {
+    if (missing(fs_in)){
+      stop('input fs_in is required if X is not a sensor data list.')
+    }
+    x <- X
+  }
+  
+  if (!is.matrix(x)) {
+    x <- matrix(x, ncol = 1)
+  }
+  if (nrow(x) == 1) {
+    x <- t(x)
+  }
+  
+  if (is.list(Z)) {
+    z <- Z$data
+    fs_out <- Z$sampling_rate
+  } else {
+    if (missing(fs_out)){
+      stop('input fs_out is required if Z is not a sensor data list.')
+    }
+    z <- Z
+  }
+  
+  if (!is.matrix(z)) {
+    z <- matrix(z, ncol = 1)
+  }
+  if (nrow(z) == 1) {
+    z <- t(z)
+  }
+  
+  if (is.null(n_out)){
+    n_out <- nrow(z)
+  }
+  
+  # DO INTERPOLATION ---------------------------------
+  
+  if (fs_in == fs_out){
+    # if sampling rates are the same, no need to interpolate,
+    # just make sure the length is right
+    y <- check_size(x, n_out)
+  }else{
+    # if sampling rates are different
+    y <- matrix(0, nrow = nrow(z), ncol = ncol(x))
+    for (c in 1:ncol(x)){
+      y[ , c] <- approx(x = c(0:(nrow(x)-1)) / fs_in, 
+                y = x[, c], 
+                xout = c(0:(nrow(z)-1)) / fs_out,
+                rule = 2 # return value at the closest data extreme when extrapolating (should be only a few samples)
+                )$y
+    }
+    y <- check_size(y, n_out)
+  }
+  
+# FORMAT OUTPUT (TO SENSOR LIST IF NEEDED) ----------
+  
+  if (is.list(X)){
+    Y <- X
+    Y$data <- y
+    Y$sampling_rate <- fs_out
+    Y$history <- paste(Y$history, ' interp2length from', fs_in, 'Hz to ', fs_out, 'Hz')
+  }else{
+    Y = y
+  }
+  
+  return(Y)
+}
+
+check_size <- function(y, n_out){
+  if (nrow(y) < n_out){
+    y <- rbind(y,
+               matrix(data = y[nrow(y),],
+                      nrow = n_out - nrow(y),
+                      byrow = TRUE))
+  }
+  if (nrow(y) > n_out){
+    y <- y[1:n_out,]
+  }
+  return(y)
+}
--- R/tagtools/R/speed_from_depth.R
+++ R/tagtools/R/speed_from_depth.R
@@ -4,46 +4,63 @@
 #' 
 #' @param p The depth vector (a regularly sampled time series) in meters. sampled at sampling_rate Hz. This can either be an animaltags sensor list, or a vector.
 #' @param A (optional) A matrix or animaltags sensor data list containing acceleration data. If A is not provided then only vertical velocity is returend (same output as depth_rate()). Acceleration can be in any consistent unit, e.g., g or m/s^2. Acceleration data must have the same number of rows as p.
-#' @param sampling_rate (optional) The sampling rate of p and A in Hz (samples per second). Required only if p and A are vectors/matrices rather than sensor data lists.
+#' @param fs_p (optional) The sampling rate of p in Hz (samples per second). Required only if p is vector rather than sensor data list.
+#' @param fs_A (optional) The sampling rate of A in Hz (samples per second). Required only if A is vector rather than sensor data list.
 #' @param fc (optional) Specifies the cut-off frequency of a low-pass filter to apply to p after computing depth-rate and to A before computing pitch. The filter cut-off frequency is in Hz. The filter length is 4*sampling_rate/fc. Filtering adds no group delay. If fc is empty or not given, the default value of 0.2 Hz (i.e., a 5 second time constant) is used.
 #' @param plim (optional) Minimum pitch angle, in radians, at which speed can be computed. Default: 0.3490659 radians = 20 degrees. Errors in speed estimation using this method increase strongly at low pitch angles. To avoid estimates with poor accuracy being used in later analyses, speed estimates at low pitch angles are replaced by NaN (not-a-number). The default threshold for this is 20 degrees.
-#' @return A list with 2 elements:
+#' @return Either forward speed or vertical speed:
 #' \itemize{
-#' \item{\strong{s: }}The forward speed estimate in m/s (only returned if acceleration input data is provided)
-#' \item{\strong{v: }} The depth-rate (or vertical velocity) in m/s
+#' \item{\strong{s: }} If both \code{p} and \code{A} are input, the forward speed estimate in m/s is returned
+#' \item{\strong{v: }} If only \code{p} is input, the depth-rate (or vertical velocity) in m/s is returned
 #' }
-#' @note Output sampling rate is the same as the input sampling rate so s and v have the same size as p.
+#' @note Output sampling rate is the same as the input sampling rate. If A and p are input and A has a higher sampling rate, then p and the output are interpolated to match A using \code{\link[tagtools]{interp2length}} .
 #' @note Frame: This function assumes a [north,east,up] navigation frame and a [forward,right,up] local frame. In these frames, a positive pitch angle is an anti-clockwise rotation around the y-axis. A descending animal will have a negative pitch angle.
+#' @examples 
+#'   s <- speed_from_depth(harbor_seal$P, harbor_seal$A)    
 #' @export
 
-speed_from_depth <- function(p, A, sampling_rate, fc = 0.2, plim = 20/180*pi) {
+speed_from_depth <- function(p, A = NULL, fs_p = NULL, fs_A = NULL, fc = 0.2, plim = 20/180*pi) {
   # input checks-----------------------------------------------------------
   if (missing(p)) {
     stop("input p is required for speed_from_depth()")
   }
   
-  if (is.list(p) && is.list(A)) {
-    if (p$sampling_rate != A$sampling_rate){
-      stop("p and A must have the same sampling rate for speed_from_depth().")
-    }
-    sampling_rate <- p$sampling_rate 
-    p <- p$data
+  if (is.list(A)){
+    fs_A <- A$sampling_rate
     A <- A$data
-  } else {
-    if (missing(sampling_rate)) {
-      stop("sampling_rate required for vector/matrix sensor data inputs to speed_from_depth()")
-    }
   }
   
-  v = depth_rate(p=p, fs=sampling_rate, fc=fc)
-  if (!missing(A)){ #if both p and A are input
+  if (is.list(p)){
+    fs_p <- p$sampling_rate
+    p <- p$data
+  }
+  
+  if (is.null(fs_p)){
+    stop('input fs_p is required for speed_from_depth(), if p is not a sensor data list.')
+  }
+  
+  # make sure p and A data are same sampling rate and same size
+  if (!is.null(A)){
+    p <- interp2length(X = p, Z = A, fs_in = fs_p, fs_out = fs_A)
+    sampling_rate <- fs_A
+  }else{
+    sampling_rate <- fs_p
+  }
+
+  v = depth_rate(p = p, 
+                 fs = sampling_rate, 
+                 fc = fc)    
+  if (is.null(A)){
+    # if only depth is input, get vertical speed
+    return(v)
+  }else{
+    # if both depth and accel are input
+    # compute pitch and get forward speed
     nf <- round(4 * sampling_rate / fc)
     A <- fir_nodelay(A, nf, fc / (sampling_rate / 2))
     pitch <- a2pr(A)$p
     pitch[abs(pitch) < plim] = NA
     s <- v / sin(pitch)
-  } else {
-    s <- NA
+    return(s)
   }
-  return(list(s = s, v = v))
 }
