008b627524d2de366db109eface5d886a0a14db5
Stacy DeRuiter <stacy@calvin.edu>
2021-06-17 09:15:27 -0400

    few more documentation and examples corrections

--- R/tagtools/R/lalo2llf.R
+++ R/tagtools/R/lalo2llf.R
@@ -4,9 +4,6 @@
 #' @param pt c(latitude, longitude) of the centre point of the local level frame. If pt is not given, the first point in the track will be used.
 #' @return A data frame with columns \code{northing} and \code{easting} of track points in the local level frame. Northing and easting are in metres. The axes of the frame are true (geographic) north and true east.
 #' @export
-#' @examples \dontrun{
-#'
-#' }
 #' @note This function assumes the track is on the surface of the geoid,
 #'  and also uses a simple spherical model for the geoid. For
 #'  more accurate conversion to a Cartesian frame, use spatial and mapping packages in Matlab/Octave.
