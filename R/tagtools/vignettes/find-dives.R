1e94db18892aeed589240fff9ebfca1c49713e8e
sbf5calvin <sbf5@students.calvin.edu>
2021-06-18 15:00:28 -0400

    Finishing up find-dives
    Once again, if the .html or .R files are problematic, please delete right away. They just showed up.

--- R/tagtools/vignettes/find-dives.R
+++ R/tagtools/vignettes/find-dives.R
@@ -10,3 +10,43 @@ knitr::opts_chunk$set(
 ## ---- eval = FALSE------------------------------------------------------------
 #  MN <- load_nc('mn12_186a_raw')
 
+## ---- eval = FALSE------------------------------------------------------------
+#  names(MN)
+#  str(MN$A)
+#  # not run because output is very long! see the whole STRucture of MN:
+#  # str(MN)
+#  # shorter outline version:
+#  str(MN, max.level = 1)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  plott(X=list(Depth=MN$P, Temperature=MN$T), r=c(TRUE,FALSE))
+
+## ---- eval = FALSE------------------------------------------------------------
+#  Pc = crop(MN$P)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  Pc$history
+#  str(Pc, max.level = 1)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  plott(X = list(Pc))
+#  
+
+## ---- eval = FALSE------------------------------------------------------------
+#  Tc <- crop_to(MN$T,tcues=Pc$crop)$X
+
+## -----------------------------------------------------------------------------
+? fix_pressure
+
+## ---- eval = FALSE------------------------------------------------------------
+#  Pcmf <-fix_pressure(Pcm,Tc)$p
+
+## ---- eval = FALSE------------------------------------------------------------
+#  d <-find_dives(Pcmf,mindepth=5)
+#  head(d)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  plott(X=list(Pcmf=Pcmf), r=TRUE)
+#  points(d$start/(3600*24),rep(0,nrow(d)),col='green', pch=19)
+#  points(d$end/(3600*24),rep(0,nrow(d)),col='red', pch=17)
+
