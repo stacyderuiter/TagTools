8ce8a84697b0d97239477fa166793be1b993bd89
Danuta Maria Wisniewska <dmwisniewska@gmail.com>
2021-07-05 15:44:25 +0200

    Moved app2plot test script to the root Python directory.
    The script uses functions from several sub-folders/packages, so it should be easier to keep track of and update when it's on the root.

--- Python/app2plot.py
+++ Python/app2plot.py
@@ -1,5 +1,5 @@
 # from tagiofuns import load_nc
-from load_nc import load_nc
+from tagiofuns.load_nc import load_nc
 import matplotlib.pyplot as plt
 
 X = load_nc()
@@ -7,7 +7,7 @@ X = load_nc()
 # plt.plot(X['P']['data'])
 # plt.show()
 
-from plott import plott
+from plotting.plott import plott
 # fig, axes, h = plott(X['P'])
 # print(h)
 # plott(X['P'])
